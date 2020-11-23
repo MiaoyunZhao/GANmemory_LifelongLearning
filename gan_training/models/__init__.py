fb91a32937b87e675805d09e652431eb5006dcf2
Miaoyun Zhao <miaoyun9zhao@gmail.com>
2020-11-23 09:18:51 +0800

    Update __init__.py

--- gan_training/models/__init__.py
+++ gan_training/models/__init__.py
@@ -1,19 +1,22 @@
 from gan_training.models import (
     resnet4,
     resnet4_AdaFM_bias,
+    resnet4_AdaFM_bias_classCondition,
     resnet4_AdaFM_accumulate_multitasks,
 )
 
 generator_dict = {
     'resnet4': resnet4.Generator,
     'resnet4_AdaFM_bias': resnet4_AdaFM_bias.Generator,
-    resnet4_AdaFM_accumulate_multitasks.Generator,
+    'resnet4_AdaFM_bias_classCondition': resnet4_AdaFM_bias_classCondition.Generator,
+    'resnet4_AdaFM_accumulate_multitasks': resnet4_AdaFM_accumulate_multitasks.Generator,
 }
 
 discriminator_dict = {
     'resnet4': resnet4.Discriminator,
     'resnet4_AdaFM_bias': resnet4_AdaFM_bias.Discriminator,
-    resnet4_AdaFM_accumulate_multitasks.Discriminator,
+    'resnet4_AdaFM_bias_classCondition': resnet4_AdaFM_bias_classCondition.Discriminator,
+    'resnet4_AdaFM_accumulate_multitasks': resnet4_AdaFM_accumulate_multitasks.Discriminator,
 }
 
 encoder_dict = {
